8af77dba94eb59f67587736866cc442813c4ef7e
Roey Brecher <roey.brecher@gmail.com>
2021-01-25 20:01:15 +0200

    using geo location

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
     const page = await browser.newPage()
+
+    await page.evaluateOnNewDocument(function() {
+        navigator.geolocation.getCurrentPosition = function (cb) {
+            setTimeout(() => {
+                cb({
+                    'coords': {
+                        accuracy: 21,
+                        altitude: null,
+                        altitudeAccuracy: null,
+                        heading: null,
+                        latitude: 32.07948174948672,
+                        longitude: 34.79441454362365,
+                        speed: null
+                    }
+                })
+            }, 1000)
+        }
+    });
+
     await page.goto('https://app.meckano.co.il/login.php')
     await page.type('#email', process.env.MECKANO_USER)
     await page.type('#password', process.env.MECKANO_PASS)
@@ -12,6 +31,10 @@ const puppeteer = require('puppeteer');
     await page.waitForNavigation()
 
     //Checkout
-    await page.click('#checkout-button')
+    await page.waitForSelector('.wrapperCheckout')
+    await page.click('.wrapperCheckout')
+
+    await new Promise(r => setTimeout(r, 10000));
+
     await browser.close()
-})().catch((exception) => console.log(exception))
+})().catch((exception) => console.log(exception)).finally(() => process.exit())
